b503309534c96fad3d72b73974bbc7773dd59c38
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2019-08-31 11:46:52 +0200

    main: open given library file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use dylib::DynamicLibrary;
+
 use std::env;
 use std::path::Path;
 
@@ -9,4 +11,7 @@ fn main() {
     let filepath = Path::new(&filename);
 
     println!("Opening library {}", filename);
+
+    let plugin = DynamicLibrary::open(Some(filepath))
+        .expect(&format!("Could not open library {}", filename));
 }
