22f865efe962ceedafa0e3ad7dc128b9497fd926
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2019-08-31 14:03:27 +0200

    WIP: use dlopen instead of dylib

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
 #![feature(c_variadic)]
-use dylib::DynamicLibrary;
+
+use dlopen::symbor::{Library, Symbol, SymBorApi};
 
 use std::env;
 use std::path::Path;
 
-
 mod vlc
 {
-    use std::ffi::{VaList};
+    use std::ffi::{VaList, CStr};
     use libc::{c_void, c_int, c_char};
-    pub type SetFunc   = unsafe extern fn (*mut c_void, *mut c_void, c_int, VaList) -> c_int;
-    pub type EntryFunc = unsafe extern fn (SetFunc, *mut c_void) -> c_int;
-    pub type MetaExportFunc = unsafe extern fn () -> *const c_char;
+    use dlopen::symbor::{Symbol, SymBorApi};
+    pub type SetFunc   = unsafe extern "C" fn (*mut c_void, *mut c_void, c_int, VaList) -> c_int;
+    pub type EntryFunc = unsafe extern "C" fn (SetFunc, *mut c_void) -> c_int;
+    pub type MetaExportFunc = unsafe extern "C" fn () -> *const c_char;
+
+    #[derive(dlopen_derive::SymBorApi)]
+    pub struct PluginEntry<'a>
+    {
+        #[dlopen_name="vlc_entry_api_version"]
+        sym_api_version: Symbol<'a, MetaExportFunc>,
+        #[dlopen_name="vlc_entry"]
+        sym_entry: Symbol<'a, EntryFunc>,
+        #[dlopen_name="vlc_entry_copyright"]
+        sym_copyright: Symbol<'a, MetaExportFunc>,
+        #[dlopen_name="vlc_entry_license"]
+        sym_license: Symbol<'a, MetaExportFunc>,
+    }
+
+    /**
+     * Safe wrappers around dll-defined symbols
+     **/
+    impl<'a> PluginEntry<'a>
+    {
+        /**
+         * Return the version of libvlccore which the loaded plugin was compiled against.
+         **/
+        pub fn api_version(&self) -> String
+        {
+            unsafe
+            {
+                CStr::from_ptr((self.sym_api_version)())
+                    .to_string_lossy()
+                    .into()
+            }
+        }
+    }
+
 }
+
 
 fn main()
 {
@@ -20,25 +55,18 @@ fn main()
         .skip(1).next()
         .expect("Usage: vlc-module-tool <filename>");
 
-    let filepath = Path::new(&filename);
-
     println!("Opening library {}", filename);
 
-    let plugin = DynamicLibrary::open(Some(filepath))
+    let plugin = Library::open(&filename)
         .expect(&format!("Could not open library {}", filename));
 
     /* In VLC 3, these symbols were:
      *  - vlc_entry_license__3_0_0f
      *  - vlc_entry_copyright__3_0_0f
      *  - vlc_entry__3_0_0f  */
-    unsafe {
-        let api_version = plugin.symbol::<vlc::MetaExportFunc>("vlc_entry_api_version")
-            .unwrap();
-        let entry       = plugin.symbol::<vlc::EntryFunc>("vlc_entry")
-            .unwrap();
-        let copyright   = plugin.symbol::<vlc::MetaExportFunc>("vlc_entry_copyright")
-            .unwrap();
-        let license     = plugin.symbol::<vlc::MetaExportFunc>("vlc_entry_license")
-            .unwrap();
-    }
+    let mut plugin_interface = unsafe { vlc::PluginEntry::load(&plugin) }.unwrap();
+
+    let api_version = plugin_interface.api_version();
+
+    println!("LibVLCCore Version: {}", api_version);
 }
