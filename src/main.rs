64cd8633448640f849dfb306ab82cf5f86990fc0
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2019-08-31 11:43:31 +0200

    main: extract first argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args()
+        .skip(1).next()
+        .expect("Usage: vlc-module-tool <filename>");
+
+    let filepath = Path::new(&filename);
+
+    println!("Opening library {}", filename);
 }
